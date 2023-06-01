c056320a85a847814622034820573abbbbdadfb5
chpancrate <chpancrate@gmail.com>
2023-06-01 16:09:24 +0200

    authentication backbone

--- litreview/urls.py
+++ litreview/urls.py
@@ -16,7 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
+
+import authentication.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        LoginView.as_view(
+            template_name="authentication/login.html", redirect_authenticated_user=True
+        ),
+        name="login",
+    ),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentication/password_change_form.html'),
+         name='password_change'
+         ),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+         name='password_change_done'
+         ),
+    path('signup/', authentication.views.signup_page, name='signup'),
 ]
